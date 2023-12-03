fba009a0d6905ada47d79c86154c0df76161d27a
Elgadisalma <elgadi.salma0@gmail.com>
2023-12-03 16:30:00 +0100

    Update edit_me.php

--- CLIENT/include/edit_me.php
+++ CLIENT/include/edit_me.php
@@ -13,42 +13,18 @@ if (isset($_POST['submit'])) {
     $nom_utilisateur = $_POST['nom_utilisateur'];
     $numero_tel = $_POST['numero_tel'];
 
-    //verifie tswira
-    if(isset($_FILES['photo']) && $_FILES['photo']['error'] === UPLOAD_ERR_OK) {
-        $photo = $_FILES['photo']['name'];
-
-        $targetDir = "./uploads/";
-
-        $targetPath = $targetDir . $photo;
-
-        $imageFileType = strtolower(pathinfo($targetPath, PATHINFO_EXTENSION));
-        $allowedExtensions = array("jpg", "jpeg", "png", "gif");
-
-        if (in_array($imageFileType, $allowedExtensions)) {
-            if (move_uploaded_file($_FILES["photo"]["tmp_name"], $targetPath)) {
-                echo "The file has been uploaded.";
-            } else {
-                echo "Sorry, there was an error ";
-            }
-        } else {
-            echo "Sorry, only JPG, JPEG, PNG, and GIF files are allowed.";
-        }
-    } else {
-        $photo = $_SESSION['photo'];
-    }
-
-    $sql = "UPDATE utilisateur SET nom_utilisateur = ?, numero_tel = ?, photo = ? WHERE id = ?";
+    
+    $sql = "UPDATE utilisateur SET nom_utilisateur = ?, numero_tel = ? WHERE id = ?";
     $stmt = $link->prepare($sql);
 
-    $stmt->bind_param("sssi", $nom_utilisateur, $numero_tel, $photo, $id);
+    $stmt->bind_param("ssi", $nom_utilisateur, $numero_tel, $id);
 
     $res = $stmt->execute();
-    // var_dump($res);
+    
 
     if ($res) {
         $_SESSION['nom_utilisateur'] = $nom_utilisateur;
         $_SESSION['numero_tel'] = $numero_tel;
-        $_SESSION['photo'] = $photo;
 
         header("location: ../pages/profil.php?STATUS=informations éditées avec succès");
         exit();
